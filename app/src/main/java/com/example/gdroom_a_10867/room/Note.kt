d368efbb3f42f0d1195c294e773836b981d35c16
valentVasti <valentvasti2114@gmail.com>
2022-09-20 13:44:37 +0700

    Upload Guided 5 Room

---
+++ app/src/main/java/com/example/gdroom_a_10867/room/Note.kt
@@ -0,0 +1,12 @@
+package com.example.gdroom_a_10867.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Note (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val title: String,
+    val note: String
+)
